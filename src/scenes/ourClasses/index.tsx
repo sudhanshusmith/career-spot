d711d09bc62bc013fcb4d6ae8c904dbe08fc3c4c
Sudhanshu Kumar <97729053+sudhanshusmith@users.noreply.github.com>
2024-07-07 09:44:32 +0530

    build error resolved

--- src/scenes/ourClasses/index.tsx
+++ src/scenes/ourClasses/index.tsx
@@ -36,7 +36,7 @@ const OurClasses = ({ setSelectedPage }: Props) => {
   return (
     <section id="ourclasses" className="w-full bg-primary-100 py-40">
       <motion.div
-        onViewportEnter={() => setSelectedPage(SelectedPage.OurClasses)}
+        // onViewportEnter={() => setSelectedPage(SelectedPage.OurClasses)}
       >
         <motion.div
           className="mx-auto w-5/6"
